f0317af37efe1712efa770dfb67d59e18ce881ab
Mayank Mittal <mayankm.iitk@gmail.com>
2019-04-05 17:11:05 +0200

    moved packages to lib directory

---
+++ lib/setup.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Mayank Mittal. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Installation script for the 'deeptam_tracker' python package."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+from setuptools import setup, find_packages
+
+# Minimum dependencies required prior to installation
+INSTALL_REQUIRES = [
+    'minieigen',
+    'numpy',
+    'scipy',
+    'matplotlib', 
+    'scikit-image',
+    'tensorflow-gpu==1.9.0'
+]
+
+# Installation operation
+setup(name='deeptam_tracker',
+      version='0.0.0',
+      description='Tracking and Mapping using Deep Learning',
+      keywords=["robotics", "machine learning", "visual odometry"],
+      packages=[package for package in find_packages() if package.startswith('deeptam')],
+      install_requires=INSTALL_REQUIRES,
+      zip_safe=False)
+
+# EOF
