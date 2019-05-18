77fdec01394c28ab37edde23ea4e72e0d2d923cb
Fadhil Ginting <fadhil.ginting@gmail.com>
2019-05-18 17:32:47 +0200

    Adding image cropper script

--- utils/bag_extractor/README.md
+++ utils/bag_extractor/README.md
@@ -10,3 +10,10 @@ python rosbag_extractor.py --rosbag_path <path/to/file.bag> --output_path <path/
 Output directory will be created if it doesn't exist.
 
 Currently, only cameras 0, 2, 4 and 6 are being used. Camera 8 is not synchronized with the rest. If you need to change the cameras to use, look at line 13 of the `rosbag_extractor.py`
+
+## Image Cropper for Multicamera
+```bash
+python image_cropper.py --input_path <path/to/input/dir> --output_path <path/to/output/dir>
+```
+Input directory path is the output path from the rosbag_extractor.py
+Output directory will be created if it doesn't exist.
--- utils/dataset_collection/apriltag/tfwriter/src/tfwriter.cpp
+++ utils/dataset_collection/apriltag/tfwriter/src/tfwriter.cpp
@@ -2,6 +2,7 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Header.h"
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <Eigen/StdVector>
@@ -9,9 +10,15 @@
 #include <Eigen/Dense>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-
+#include "tf_writer/AprilTagDetection.h"
 #include <iostream>
 #include <fstream>
+
+//void tagCallback(const tf_writer::AprilTagDetectionArray& msg) {
+////    if(msg.detections[0==19){
+//        ROS_INFO("Yes cam to 19");
+////    }
+//}
 
 int main(int argc, char **argv)
 {
@@ -19,59 +26,63 @@ int main(int argc, char **argv)
   Eigen::Translation<double,3> pos_cam_tag;
   Eigen::Quaterniond rot_cam_tag;
   Eigen::Transform<double,3,Eigen::Affine> pose_cam_tag;
-  long int timecounter=0;
+
+//  tf_writer::AprilTagDetectionArray msg_tagpose;
 
   ros::init(argc, argv, "tfwriter_node");
   ros::NodeHandle nh;
 
-  tf2_ros::Buffer tfBuffer;
-  tf2_ros::TransformListener tfListener(tfBuffer);
-  ros::Rate rate(10.0);
+//  tf2_ros::Buffer tfBuffer;
+//  tf2_ros::TransformListener tfListener(tfBuffer);
+//  ros::Rate rate(10.0);
   std::ofstream myfile;
 
   myfile.open ("groundtruth.txt");
   myfile << "#  Timestamps tx, ty, tz, qx, qy, qz, qw\n";
+//  ros::Subscriber posesubs = nh.subscribe("tag_detections",10,tagCallback);
 
-  while (nh.ok()) {
-      geometry_msgs::TransformStamped tfstamped;
-      try {
-          //get tf camera to tag 15
-          tfstamped = tfBuffer.lookupTransform("camera",
-          "tag_19", ros::Time(0));
+//  while (nh.ok()) {
 
-          pos_cam_tag.x()=tfstamped.transform.translation.x;
-          pos_cam_tag.y()=tfstamped.transform.translation.y;
-          pos_cam_tag.z()=tfstamped.transform.translation.z;
-          rot_cam_tag.w() = tfstamped.transform.rotation.w;
-          rot_cam_tag.vec() << tfstamped.transform.rotation.x,
-                  tfstamped.transform.rotation.y,
-                  tfstamped.transform.rotation.z;
-          long int timestamp_sec = tfstamped.header.stamp.sec;
-          long int timestamp_nsec = tfstamped.header.stamp.nsec;
+////      geometry_msgs::TransformStamped tfstamped;
+////      try {
+////          //get tf camera to tag 15
+////          tfstamped = tfBuffer.lookupTransform("camera",
+////          "tag_19", ros::Time(0));
 
-          ROS_INFO_STREAM(tfstamped.transform.translation.x<<" "<<tfstamped.transform.translation.y<<" "<<tfstamped.transform.translation.z);
+//          pos_cam_tag.x()=tfstamped.transform.translation.x;
+//          pos_cam_tag.y()=tfstamped.transform.translation.y;
+//          pos_cam_tag.z()=tfstamped.transform.translation.z;
+//          rot_cam_tag.w() = tfstamped.transform.rotation.w;
+//          rot_cam_tag.vec() << tfstamped.transform.rotation.x,
+//                  tfstamped.transform.rotation.y,
+//                  tfstamped.transform.rotation.z;
+//          long int timestamp_sec = tfstamped.header.stamp.sec;
+//          long int timestamp_nsec = tfstamped.header.stamp.nsec;
 
-          //Get 4x4 transform
-          pose_cam_tag = pos_cam_tag * rot_cam_tag;
-          //Inverse to get tag to camera
-          pose_cam_tag = pose_cam_tag.inverse();
-          Eigen::Quaterniond rot_tag_cam(pose_cam_tag.rotation());
+////          ROS_INFO_STREAM(tfstamped.transform.translation.x<<" "<<tfstamped.transform.translation.y<<" "<<tfstamped.transform.translation.z);
 
-          myfile << timestamp_sec <<"" << timestamp_nsec << " "
-                 << pose_cam_tag.translation()[0] << " " << pose_cam_tag.translation()[1] << " " << pose_cam_tag.translation()[2] << " "
-                 << rot_tag_cam.x() << " " << rot_tag_cam.y() << " " << rot_tag_cam.z() << " " << rot_tag_cam.w()
-                 << "\n";
+//          //Get 4x4 transform
+//          pose_cam_tag = pos_cam_tag * rot_cam_tag;
+//          //Inverse to get tag to camera
+//          pose_cam_tag = pose_cam_tag.inverse();
+//          Eigen::Quaterniond rot_tag_cam(pose_cam_tag.rotation());
 
-          timecounter++;
+//          myfile << timestamp_sec <<"" << timestamp_nsec << " "
+//                 << pose_cam_tag.translation()[0] << " " << pose_cam_tag.translation()[1] << " " << pose_cam_tag.translation()[2] << " "
+//                 << rot_tag_cam.x() << " " << rot_tag_cam.y() << " " << rot_tag_cam.z() << " " << rot_tag_cam.w()
+//                 << "\n";
 
-          }
-      catch (tf2::TransformException &exception) {
-          ROS_WARN("%s", exception.what());
-          ros::Duration(1.0).sleep();
-          continue;
-      }
-      rate.sleep();
-  }
+//          timecounter++;
+
+//          }
+//      catch (tf2::TransformException &exception) {
+//          ROS_WARN("%s", exception.what());
+//          ros::Duration(1.0).sleep();
+//          continue;
+//      }
+//      rate.sleep();
+//  }
+  ros::spin();
   myfile.close();
   return 0;
 }
